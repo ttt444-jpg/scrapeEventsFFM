3e5085acb716aa70cc8aced96fbd01cd6973666b
thomas <thomas.hofmann@fra-uas.de>
2026-07-16 13:56:04 +0200

    Revert "schlachthof image WIP"
    This reverts commit 190435b0578fd23e30f0e4014ebcd08674f5d5e0.

--- scraper/sites/schlachthof.js
+++ scraper/sites/schlachthof.js
@@ -1,48 +1,40 @@
 import { loadPage } from "../scraperBase.js";
 
 export async function scrapeSchlachthof() {
-  const baseUrl = "https://schlachthof-wiesbaden.de/";
-  const $ = await loadPage(baseUrl);
+  const url = "https://schlachthof-wiesbaden.de/";
+  const $ = await loadPage(url);
 
   const events = [];
 
-  const items = $("a.border-t.block.group").toArray();
-
-  for (const el of items) {
+  $("a.border-t.block.group").each((_, el) => {
     const item = $(el);
 
+    // Link
     const link = item.attr("href");
-    const fullLink = link.startsWith("http") ? link : baseUrl + link;
 
+    // Datum (1–2-stellige Zahl)
     const date = item.find("div").filter((_, div) => {
       const text = $(div).text().trim();
       return /^\d{1,2}$/.test(text);
     }).text().trim();
 
+    // Titel
     const title = item.find("h2").text().trim();
+
+    // Beschreibung / Excerpt
     const excerpt = item.find("div.mt\\[10px\\]").text().trim();
-
-    // Detailseite laden (jetzt funktioniert await!)
-    const detail$ = await loadPage(fullLink);
-
-    const imgSrc = detail$("div.aspect-video img").attr("src");
-
-    const image = imgSrc
-      ? (imgSrc.startsWith("http") ? imgSrc : baseUrl + imgSrc)
-      : null;
 
     events.push({
       date,
       title,
       excerpt,
-      link: fullLink,
-      image,
+      link,
     });
-  }
+  });
 
   return {
     site: "Schlachthof Wiesbaden",
-    url: baseUrl,
+    url,
     events,
   };
 }
