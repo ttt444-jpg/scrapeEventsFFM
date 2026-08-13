7821c8fbf7886663fe7da496f02b303818f3b654
thomas <thomas.hofmann@fra-uas.de>
2026-08-13 22:58:53 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ import { ocrFromUrl } from "./utils/ocr.js";
 import { llava } from "./utils/ollamaRunner.js";
 import { llama32 } from "./utils/ollamaRunner.js";
 
- runScraper();
+setTimeout(() => {
+  runScraper();
+}, 1000);
 // ocrFromUrl("https://berlin.ccc.de/img/logo.png");
 //llava();
 //llama32();
