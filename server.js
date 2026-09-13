c789fdd68c53bf1586446e49fc8335b37eb18f9e
thomas <thomas.hofmann@fra-uas.de>
2026-09-13 14:57:27 +0200

    added favicon

--- server.js
+++ server.js
@@ -107,6 +107,7 @@ app.get("/", (req, res) => {
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>069 EVENTS</title>
+        <link rel="icon" type="image/png" href="/favicon.png?v=1">
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Geist:wght@400;500;600;700&family=Geist+Mono:wght@400;500&display=swap" rel="stylesheet">
